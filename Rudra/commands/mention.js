296085899d5bf2b85449f0fd0f089fb3c4e0624b
SHAHBOT235 <khobsortlarka@gmail.com>
2025-08-19 15:06:43 +0500

    Update mention.js

--- Rudra/commands/mention.js
+++ Rudra/commands/mention.js
@@ -9,11 +9,11 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "61550558518720") {
-    var aid = ["61550558518720"];
+  if (event.senderID !== "61553565459843") {
+    var aid = ["61553565459843"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "  Bhai Rudra Toh Chala gaya"];
+      var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "  Bhai Haseen Mohtarm Toh Chala gaya offline"];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
